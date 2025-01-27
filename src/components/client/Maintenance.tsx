f315db4435877e4409ef7c2bfa75d86ed063b416
LaMuerte95 <juanemail2001@gmail.com>
2025-01-27 16:26:11 -0600

    Added logo and name

--- src/components/client/Maintenance.tsx
+++ src/components/client/Maintenance.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Image from "next/image";
+import icon from "../../app/icon.png"
 interface UnderMaintenanceProps {
   message?: string;
 }
@@ -10,6 +11,10 @@ export const UnderMaintenance: React.FC<UnderMaintenanceProps> = ({
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="text-center p-6 rounded-lg bg-white shadow-md">
+
+        <div className="flex justify-center m-4">
+          <Image src={icon} alt="App Icon" width={100} height={100} />
+        </div>
         <h1 className="text-4xl font-bold text-gray-800 mb-4">🚧 Under Maintenance 🚧</h1>
         <p className="text-gray-600 text-lg">{message}</p>
         <p className="text-sm text-gray-500 mt-4">
